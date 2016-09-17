176df3073c151e4e0d165b3bd2af8bc264a6b472
notsyncing <song.fc@gmail.com>
2016-09-17 17:01:02 +0800

    修复了解析BeaconData出错的问题。

--- src/main/kotlin/io/github/notsyncing/manifold/eventbus/event/BeaconData.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/eventbus/event/BeaconData.kt
@@ -3,9 +3,13 @@ package io.github.notsyncing.manifold.eventbus.event
 import io.github.notsyncing.manifold.eventbus.ManifoldEventNode
 
 class BeaconData {
-    var id: String
+    lateinit var id: String
     var groups: Array<String> = emptyArray()
-    var load: Int
+    var load: Int = 0
+
+    constructor() {
+
+    }
 
     constructor(node: ManifoldEventNode) {
         this.id = node.id
