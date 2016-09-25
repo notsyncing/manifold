27dd29ee4a2f9a23add6fba30aa8b1f2c88c81d8
0xfc <song.fc@gmail.com>
2016-09-25 09:56:44 +0800

    补充网络接收消息的测试用例。

--- src/test/kotlin/io/github/notsyncing/manifold/tests/EventBusNetWorkerTest.kt
+++ src/test/kotlin/io/github/notsyncing/manifold/tests/EventBusNetWorkerTest.kt
@@ -5,7 +5,9 @@ import io.github.notsyncing.manifold.eventbus.ManifoldEventNode
 import io.github.notsyncing.manifold.eventbus.event.ManifoldEvent
 import io.github.notsyncing.manifold.utils.ReadInputStream
 import io.github.notsyncing.manifold.utils.StreamUtils
+import io.github.notsyncing.manifold.utils.WriteOutputStream
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import org.junit.After
@@ -14,22 +16,26 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.ByteArrayInputStream
 import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.TimeUnit
 
 @RunWith(VertxUnitRunner::class)
 class EventBusNetWorkerTest {
     private lateinit var vertx: Vertx
     private lateinit var fakeEventNode: ManifoldEventNode
     private lateinit var worker: EventBusNetWorker
+    private var recvEvent: CompletableFuture<ManifoldEvent> = CompletableFuture()
 
     @Before
     fun setUp() {
         vertx = Vertx.vertx()
+        recvEvent = CompletableFuture()
 
         fakeEventNode = ManifoldEventNode("test", host = "127.0.0.1", port = 8501)
-        worker = EventBusNetWorker(8500, 8500) { ev, host -> }
+        worker = EventBusNetWorker(8500, 8500) { ev, host -> recvEvent.complete(ev) }
     }
 
     @After
@@ -204,5 +210,97 @@ class EventBusNetWorkerTest {
                 return@exceptionally false
             }
         }
+    }
+
+    private fun sendEventUdp(event: ManifoldEvent): CompletableFuture<Void> {
+        val c = CompletableFuture<Void>()
+        val s = vertx.createDatagramSocket()
+
+        s.send(Buffer.buffer(event.serialize()), 8500, "127.0.0.1") {
+            if (it.succeeded()) {
+                c.complete(null)
+            } else {
+                c.completeExceptionally(it.cause())
+            }
+
+            s.close()
+        }
+
+        return c
+    }
+
+    private fun sendEventTcp(event: ManifoldEvent, useStream: Boolean = false): CompletableFuture<Void> {
+        val c = CompletableFuture<Void>()
+        val n = vertx.createNetClient()
+
+        n.connect(8500, "127.0.0.1") {
+            if (it.failed()) {
+                c.completeExceptionally(it.cause())
+                return@connect
+            }
+
+            if (useStream) {
+                val ws = WriteOutputStream(it.result())
+                val s = DataOutputStream(ws)
+
+                event.serialize(s).thenAccept {
+                    ws.close()
+
+                    c.complete(null)
+
+                    n.close()
+                }
+            } else {
+                val buf = event.serialize()
+                it.result().write(Buffer.buffer(buf))
+
+                c.complete(null)
+
+                n.close()
+            }
+        }
+
+        return c
+    }
+
+    @Test
+    fun testRecvStringThroughUdp(context: TestContext) {
+        val event = ManifoldEvent("event", "Test data 5")
+        val async = context.async()
+
+        sendEventUdp(event).thenCompose {
+            recvEvent
+        }.thenAccept {
+            context.assertEquals("Test data 5", it.data)
+            async.complete()
+        }.get(5, TimeUnit.SECONDS)
+    }
+
+    @Test
+    fun testRecvStringThroughTcp(context: TestContext) {
+        val event = ManifoldEvent("event", "Test data 6")
+        val async = context.async()
+
+        sendEventTcp(event).thenCompose {
+            recvEvent
+        }.thenAccept {
+            context.assertEquals("Test data 6", it.data)
+            async.complete()
+        }.get(5, TimeUnit.SECONDS)
+    }
+
+    @Test
+    fun testRecvStreamThroughTcp(context: TestContext) {
+        val s = "Test data 7"
+        val event = ManifoldEvent("event", StreamUtils.stringToStream(s), s.toByteArray(StandardCharsets.UTF_8).size.toLong())
+        val async = context.async()
+
+        sendEventTcp(event, true).thenCompose {
+            recvEvent
+        }.thenAccept {
+            context.assertNotNull(it.dataStream)
+            context.assertEquals("Test data 7", StreamUtils.streamToString(it.dataStream!!, it.dataLength.toInt()))
+            async.complete()
+        }.get(5, TimeUnit.SECONDS)
     }
 }
