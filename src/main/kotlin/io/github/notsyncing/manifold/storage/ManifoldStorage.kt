a9a4958345ccde15e40b912807012d5fda528379
notsyncing <song.fc@gmail.com>
2016-10-16 14:33:22 +0800

    存储对象基类支持在未指定场景上下文时自行获取数据库操作对象。

--- src/main/kotlin/io/github/notsyncing/manifold/storage/ManifoldStorage.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/storage/ManifoldStorage.kt
@@ -1,14 +1,27 @@
 package io.github.notsyncing.manifold.storage
 
+import io.github.notsyncing.manifold.Manifold
 import io.github.notsyncing.manifold.action.ManifoldTransaction
 import io.github.notsyncing.manifold.action.SceneContext
 
 abstract class ManifoldStorage<T> {
-    lateinit var sceneContext: SceneContext
+    private var ownDb: T? = null
 
-    protected val transaction: ManifoldTransaction<T>
-        get() = sceneContext.transaction!! as ManifoldTransaction<T>
+    var sceneContext: SceneContext? = null
+
+    protected val transaction: ManifoldTransaction<T>?
+        get() = sceneContext?.transaction as ManifoldTransaction<T>?
 
     protected val db: T
-        get() = transaction.body
+        get() {
+            if (transaction != null) {
+                return transaction!!.body
+            } else {
+                if (ownDb == null) {
+                    ownDb = Manifold.transactionProvider?.get()?.body!! as T
+                }
+
+                return ownDb!!
+            }
+        }
 }
