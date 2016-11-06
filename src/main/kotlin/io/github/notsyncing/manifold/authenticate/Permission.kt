dc24e260f8748e625b0e9ffcc55e9f7e52e59d66
notsyncing <song.fc@gmail.com>
2016-11-06 21:57:53 +0800

    增加生成权限列表的一些工具方法。

--- src/main/kotlin/io/github/notsyncing/manifold/authenticate/Permission.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/authenticate/Permission.kt
@@ -3,10 +3,48 @@ package io.github.notsyncing.manifold.authenticate
 class Permission(val module: Enum<*>,
                  val type: Enum<*>,
                  val state: PermissionState = PermissionState.Undefined,
-                 val additionalData: Any? = null) {
+                 var additionalData: Any? = null) {
+    companion object {
+        class PermissionBuilderItem(private val list: MutableList<Permission>, val state: PermissionState, val module: Int) {
+            infix fun type(type: Enum<*>): Permission {
+                val p = Permission(module, type.ordinal, state)
+                list.add(p)
+
+                return p
+            }
+        }
+
+        class PermissionBuilder {
+            companion object {
+                val lets = PermissionBuilder()
+            }
+
+            val lets = Companion.lets
+            val list = mutableListOf<Permission>()
+
+            infix fun allow(module: Enum<*>): PermissionBuilderItem {
+                return PermissionBuilderItem(list, PermissionState.Allowed, module.ordinal)
+            }
+
+            infix fun deny(module: Enum<*>): PermissionBuilderItem {
+                return PermissionBuilderItem(list, PermissionState.Forbidden, module.ordinal)
+            }
+        }
+
+        fun build(b: PermissionBuilder.() -> Unit): List<Permission> {
+            val builder = PermissionBuilder()
+            builder.b()
+
+            return builder.list
+        }
+    }
+
     constructor(module: Int, type: Int, state: PermissionState = PermissionState.Undefined, additionalData: Any? = null)
             : this(SceneAuthenticator.authModuleEnumClass.enumConstants[module],
                     SceneAuthenticator.authTypeEnumClass.enumConstants[type], state, additionalData) {
+    }
 
+    infix fun additional(data: Any) {
+        this.additionalData = data
     }
 }
