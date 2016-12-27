fdd14a21365f86de67cd7afd5adc047f092abea7
notsyncing <song.fc@gmail.com>
2016-12-27 15:49:58 +0800

    为聚合权限类增加无参构造函数。

--- src/main/kotlin/io/github/notsyncing/manifold/authenticate/AggregatedPermissions.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/authenticate/AggregatedPermissions.kt
@@ -1,6 +1,8 @@
 package io.github.notsyncing.manifold.authenticate
 
 class AggregatedPermissions(val permissions: List<Permission>) {
+    constructor() : this(emptyList())
+
     fun get(module: Enum<*>, type: Enum<*>): Permission {
         val r = permissions.firstOrNull { (it.module == module) && (it.type == type) }
 
