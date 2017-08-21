7b088fe489954bc09073ad79df9691e56db8dcaf
notsyncing <song.fc@gmail.com>
2017-08-21 16:08:36 +0800

    story: 添加域支持。

--- manifold-story/src/main/kotlin/io/github/notsyncing/manifold/story/StoryLibrary.kt
+++ manifold-story/src/main/kotlin/io/github/notsyncing/manifold/story/StoryLibrary.kt
@@ -1,6 +1,7 @@
 package io.github.notsyncing.manifold.story
 
 import io.github.notsyncing.manifold.Manifold
+import io.github.notsyncing.manifold.domain.ManifoldDomain
 import kotlinx.coroutines.experimental.future.await
 import kotlinx.coroutines.experimental.future.future
 import java.util.concurrent.ConcurrentHashMap
@@ -22,6 +23,29 @@ object StoryLibrary {
                 }
 
                 afterStories[s]!!.add(it)
+            }
+        }
+
+        ManifoldDomain.afterScan {
+            it.inAllClassScanResults { s, cl ->
+                s?.getNamesOfClassesWithAnnotation(AfterStoryOf::class.java)?.forEach { className ->
+                    val clazz = cl.loadClass(className) as Class<AfterStory>
+                    val forStories = clazz.getAnnotation(AfterStoryOf::class.java).value
+
+                    for (forStory in forStories) {
+                        if (!afterStories.containsKey(forStory)) {
+                            afterStories[forStory] = mutableListOf()
+                        }
+
+                        afterStories[forStory]!!.add(clazz)
+                    }
+                }
+            }
+        }
+
+        ManifoldDomain.onClose { _, cl ->
+            afterStories.forEachValue(1) {
+                it.removeIf { it.classLoader == cl }
             }
         }
     }
