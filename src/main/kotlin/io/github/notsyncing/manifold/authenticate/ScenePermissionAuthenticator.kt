9dfa57e8e3770c6b15c7bbb1e39e9682f4b02c75
notsyncing <song.fc@gmail.com>
2017-09-21 16:18:48 +0800

    小修改。

--- src/main/kotlin/io/github/notsyncing/manifold/authenticate/ScenePermissionAuthenticator.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/authenticate/ScenePermissionAuthenticator.kt
@@ -3,8 +3,11 @@ package io.github.notsyncing.manifold.authenticate
 import io.github.notsyncing.manifold.action.interceptors.SceneInterceptorContext
 import java.util.concurrent.CompletableFuture
 
-open class ScenePermissionAuthenticator(private val authModule: Enum<*>,
-                                        private val authType: Enum<*>) : SceneAuthenticator() {
+open class ScenePermissionAuthenticator(private val authModule: String,
+                                        private val authType: String) : SceneAuthenticator() {
+    @Deprecated("Please use String to specify module and type")
+    constructor(authModule: Enum<*>, authType: Enum<*>) : this(authModule.name, authType.name)
+
     override fun authenticate(context: SceneInterceptorContext, role: AuthRole): CompletableFuture<Unit> {
         return context.checkPermission(authModule, authType)
     }
