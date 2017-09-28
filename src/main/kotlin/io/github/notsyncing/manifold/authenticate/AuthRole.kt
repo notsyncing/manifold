5362ca0c9eb8aba8566604be6c2fd07a27906a83
notsyncing <song.fc@gmail.com>
2017-09-28 13:57:24 +0800

    为认证角色类增加会话ID字段。

--- src/main/kotlin/io/github/notsyncing/manifold/authenticate/AuthRole.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/authenticate/AuthRole.kt
@@ -1,8 +1,9 @@
 package io.github.notsyncing.manifold.authenticate
 
 open class AuthRole(val roleId: String = "",
-               val groups: Array<AuthGroup> = emptyArray(),
-               permissions: Array<Permission>) : AuthItem(permissions) {
-    constructor(roleId: Long = 0, groups: Array<AuthGroup> = emptyArray(), permissions: Array<Permission>)
-            : this(if (roleId == 0L) "" else roleId.toString(), groups, permissions)
+                    val sessionId: String = "",
+                    val groups: Array<AuthGroup> = emptyArray(),
+                    permissions: Array<Permission>) : AuthItem(permissions) {
+    constructor(roleId: Long = 0, sessionId: String = "", groups: Array<AuthGroup> = emptyArray(), permissions: Array<Permission>)
+            : this(if (roleId == 0L) "" else roleId.toString(), sessionId, groups, permissions)
 }
