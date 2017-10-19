9a7b86c75609742b6e4ffae1e2041d1ba9307f04
notsyncing <song.fc@gmail.com>
2017-10-19 11:32:14 +0800

    story: 修复了Repository内的ManifoldStorage类未使用同一上下文的问题。

--- manifold-story/src/main/kotlin/io/github/notsyncing/manifold/story/drama/DramaPropertyRepository.kt
+++ manifold-story/src/main/kotlin/io/github/notsyncing/manifold/story/drama/DramaPropertyRepository.kt
@@ -1,5 +1,7 @@
 package io.github.notsyncing.manifold.story.drama
 
+import io.github.notsyncing.manifold.Manifold
+import io.github.notsyncing.manifold.storage.ManifoldStorage
 import io.github.notsyncing.manifold.utils.DependencyProviderUtils
 import java.util.concurrent.CompletableFuture
 
@@ -7,10 +9,24 @@ abstract class DramaPropertyRepository(protected val context: DramaActionContext
     init {
         context.registerRepository(this)
 
-        DependencyProviderUtils.autoProvideProperties(this)
+        DependencyProviderUtils.autoProvideProperties(this, this::provideDependency)
     }
 
     protected val sceneContext get() = context.sceneContext
+
+    private fun provideDependency(t: Class<*>): Any? {
+        if (ManifoldStorage::class.java.isAssignableFrom(t)) {
+            val o = Manifold.dependencyProvider?.get(t)
+
+            if (o != null) {
+                (o as ManifoldStorage<*>).sceneContext = sceneContext
+            }
+
+            return o
+        } else {
+            return Manifold.dependencyProvider?.get(t)
+        }
+    }
 
     open fun destroy(): CompletableFuture<Unit> {
         return CompletableFuture.completedFuture(Unit)
