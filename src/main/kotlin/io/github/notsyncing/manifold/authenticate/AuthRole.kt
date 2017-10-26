5c9cb3c271a6ef1b80187e78862191411f9ad2b7
notsyncing <song.fc@gmail.com>
2017-10-26 14:42:28 +0800

    为AuthRole类增加判断是否为超级用户的方法。

--- src/main/kotlin/io/github/notsyncing/manifold/authenticate/AuthRole.kt
+++ src/main/kotlin/io/github/notsyncing/manifold/authenticate/AuthRole.kt
@@ -6,4 +6,6 @@ open class AuthRole(val roleId: String = "",
                     permissions: Array<Permission>) : AuthItem(permissions) {
     constructor(roleId: Long = 0, sessionId: String = "", groups: Array<AuthGroup> = emptyArray(), permissions: Array<Permission>)
             : this(if (roleId == 0L) "" else roleId.toString(), sessionId, groups, permissions)
+
+    fun isSuperUser() = this == SpecialRole.SuperUser
 }
